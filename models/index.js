e95f2b54c5528c85dff6c43938ffd443ff8cb6a5
Samantha Malone <sammac99@gmail.com>
2021-07-29 11:12:32 -0700

    added product category relationship

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 // Categories have many Products
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
