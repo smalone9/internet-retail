d7c4a693d05af166ffcf6980578068e7bd84cc29
Samantha Malone <sammac99@gmail.com>
2021-07-29 13:28:17 -0700

    adding api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,16 +3,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
+
 // get all products
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  console.log('=======================');
+  Product.findAll({
+    attributes: {include: [""]}
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  console.log('=======================');
+  Product.findOne()
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
